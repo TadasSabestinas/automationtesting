711dbfa27baa2007e1265c9b1d66c82072103dd8
TadasSabestinas <90024877+TadasSabestinas@users.noreply.github.com>
2024-03-14 22:00:35 +0200

    Update and rename Test1.java to FirstTest.java

--- src/test/java/basicwebas/FirstTest.java
+++ src/test/java/basicwebas/FirstTest.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
-public class Test1 {
+public class FirstTest {
 
 	public static void main(String[] args) {
 	 System.setProperty("webdriver.chrome.driver", "Drivers\\chromeexec\\chromedriver.exe");
